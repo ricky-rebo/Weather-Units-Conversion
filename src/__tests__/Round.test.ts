03312d5418d253bcb4658d44a9002d9aa5e5d5c9
Riccardo Rebottini <ricky@rebottini.it>
2020-12-10 11:34:12 +0100

    decimal rounding test

---
+++ src/__tests__/Round.test.ts
@@ -0,0 +1,10 @@
+import round from "../rounding";
+
+test("Decimals Rounding", () => {
+  const VAL = 1.56723675219877536541;
+
+  expect(round(VAL, 1)).toBe(1.6);
+  expect(round(VAL, 3)).toBe(1.567);
+  expect(round(VAL, 5)).toBe(1.56724);
+  expect(round(VAL, 11)).toBe(1.56723675220);
+})
